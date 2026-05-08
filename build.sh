134156cb6980eaf7516b6af22f11e4b9542db646
veto8 <veto@myridia.com>
2026-05-09 03:21:30 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 wget https://github.com/sudo-give-me-coffee/wine32-deploy/releases/download/continuous/Wine-4.21-x86_64.AppImage
 chmod +x Wine-4.21-x86_64.AppImage
 ./Wine-4.21-x86_64.AppImage create-bottle  tamuz
-wget https://github.com/myridia/hello_zig/releases/download/main/hello_zig_32.exe -O tamuz/prefix/drive_c/hello_zig_32.exe
-./Wine-4.21-x86_64.AppImage set-main-executable tamuz "C:\hello_zig_32.exe"
+./Wine-4.21-x86_64.AppImage set-main-executable tamuz "C:\windows/system32/notepad.exe"    
 ./Wine-4.21-x86_64.AppImage create-appdir tamuz
 ./Wine-4.21-x86_64.AppImage test tamuz
 ./Wine-4.21-x86_64.AppImage package tamuz
